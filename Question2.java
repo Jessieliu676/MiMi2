f34a5e40e8a0e3825e8a0c31b6aad58c62204f3b
khans0618 <s2m2m2001@gmail.com>
2015-04-27 11:50:57 +0800

    A AM021980 陳思翰提交 txtCopy

--- Question2.java
+++ Question2.java
@@ -1,7 +1,27 @@
-﻿public class Question2{
+﻿import java.io.*;
+import java.util.*;
+public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
-		editFile("Dest.txt");
-		imgCopy("Source.jpg", "Dest.jpg");
+		//editFile("Dest.txt");
+		//imgCopy("Source.jpg", "Dest.jpg");
 	}
+}
+public void txtCopy(String a,String b){
+		try{
+			FileReader FR = new FileReader(a);
+			BufferedReader BR = new BufferedReader(FR);
+			FileWriter FW = new FileWriter(b);
+			BufferedWriter BW = new BufferedWriter(FW);
+			String Line;
+			while((Line=BR.readLine())!=null){
+				BW.write(Line);
+				BW.newLine();
+			}
+			BR.close();
+			FW.close();
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
 }
